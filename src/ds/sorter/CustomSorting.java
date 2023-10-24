7d98ee639ab95bfa2cecc94fea07a8e7f0f2becc
wilso <wilso@192.168.1.29>
2023-10-24 22:46:15 +1100

    Coded customer compare methods sorting by type and ripeness

--- src/ds/sorter/CustomSorting.java
+++ src/ds/sorter/CustomSorting.java
@@ -1,31 +1,43 @@
 package ds.sorter;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
-import ds.Vegetable.Vegetable;
+import ds.vegetable.Vegetable;
 
 public class CustomSorting {
-
 	
 	/**
-	 * This method must sort the vegetable alphabetically by their type. Vegetables must be sorted 
-	 *  in the order Onion, Lettuce, Tomato, Capsicum. The weight and ripeness of a vegetable should not
-	 *  be considered. 
+	 * This method must sort the vegetable alphabetically by their type.
+	 * The weight and ripeness of a vegetable should not be considered. 
 	 *  
 	 * @param list The list to sort. 
 	 * @return A sorted list. 
 	 */
 	public static List<Vegetable> sortByType(List<Vegetable> list) {
-		return null;
-		/*
-		return list.sort(new Comparator<Vegetable>() {
+		list.sort(new Comparator<Vegetable>() {
 			// Implement your custom compare method here. 
-		});
-		*/
+			
+			@Override
+			// Implement custom compare method sorting by type.
+			public int compare(Vegetable veg1, Vegetable veg2) {
+				String[] typeOrder = {"CAPSICUM", "LETTUCE", "ONION", "TOMATO"};
+				int index1 = -1;
+				int index2 = -1;
+				
+				for (int i = 0; i < typeOrder.length; i++) {
+					if (veg1.getType() == Vegetable.TYPE.valueOf(typeOrder[i])) {
+						index1 = i;
+					}
+					
+					if (veg2.getType() == Vegetable.TYPE.valueOf(typeOrder[i])) {
+						index2 = i;
+					}
+				}
+				return Integer.compare(index1, index2);
+			}
+	    });
+		return list;
 	}
-	
 	
 	/**
 	 * This method must sort the vegetable by their ripeness value, from 100% ripe to 0% ripe. 
@@ -35,11 +47,15 @@ public class CustomSorting {
 	 * @return	   A sorted list. 
 	 */
 	public static List<Vegetable> sortByRipeness(List<Vegetable> list) {
-		return null;
-		/*
-		return list.sort(new Comparator<Vegetable>() {
+		list.sort(new Comparator<Vegetable>() {
 			// Implement your custom compare method here. 
+			
+			@Override
+			public int compare(Vegetable veg1, Vegetable veg2) {
+				// Sort in descending order of ripeness.
+				return Double.compare(veg2.getRipeness(), veg1.getRipeness());
+			}
 		});
-		*/
+		return list;
 	}
 }
